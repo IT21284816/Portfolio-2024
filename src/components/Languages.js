45067323f00b1bb64451e64cc29f35b255c1e792
Duhun De Silva <sandaruduhun@gmail.com>
2024-10-25 09:37:30 +0530

    update

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,80 +1,125 @@
 import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png";
 import { Card } from 'react-bootstrap';
-import { motion } from 'framer-motion';
 import { useState } from 'react';
+import { motion } from 'framer-motion';
+// Import icons
+import javaImg from "../assets/icons/java.png";
+import reactImg from "../assets/icons/react.png";
+import javaScriptImg from "../assets/icons/java-script.png";
+import htmlImg from "../assets/icons/html.png"; 
+import phpImg from "../assets/icons/php.png";
+import mernImg from "../assets/icons/mern.png";
+import cImg from "../assets/icons/c.png"; 
+import cppImg from "../assets/icons/cpp.png"; 
+import csharpImg from "../assets/icons/csharp.png"; 
+import pythonImg from "../assets/icons/python.png"; 
+import kotlinImg from "../assets/icons/kotlin.png"; // Add the correct path
+import rImg from "../assets/icons/r.png"; 
+import typescriptImg from "../assets/icons/typescript.png"; // Add the correct path
+import nextjsImg from "../assets/icons/nextjs.png"; // Add the correct path
+import wordpressImg from "../assets/icons/wordpress.png"; // Add the correct path
+import bootstrapImg from "../assets/icons/bootstrap.png"; // Add the correct path
+import tailwindImg from "../assets/icons/tailwind.png"; // Add the correct path
+import reactNativeImg from "../assets/icons/react-native.png"; // Add the correct path
+import mysqlImg from "../assets/icons/mysql.png"; // Add the correct path
+import mongodbImg from "../assets/icons/mongodb.png"; // Add the correct path
+import firebaseImg from "../assets/icons/firebase.png"; // Add the correct path
+import oracleImg from "../assets/icons/oracle.png"; // Add the correct path
+import azureImg from "../assets/icons/azure.png"; // Add the correct path
+import androidStudioImg from "../assets/icons/android-studio.png"; // Add the correct path
+import iotImg from "../assets/icons/iot.png"; // Add the correct path
+import arduinoImg from "../assets/icons/arduino.png"; // Add the correct path
+import vmwareImg from "../assets/icons/vmware.png"; // Add the correct path
+import gitImg from "../assets/icons/git.png"; // Add the correct path
+import stripeImg from "../assets/icons/stripe.png"; // Add the correct path
+import razorImg from "../assets/icons/razor.png"; // Add the correct path
+import powerBIImg from "../assets/icons/power-bi.png"; // Add the correct path
+import fedoraImg from "../assets/icons/fedora.png"; // Add the correct path
+import centosImg from "../assets/icons/centos.png"; // Add the correct path
 
 const skills = [
-    { name: "React" },
-    { name: "JavaScript" },
-    { name: "HTML" },
-    { name: "PHP" },
-    { name: "MERN" },
-    { name: "C" },
-    { name: "C++" },
-    { name: "C#" },
-    { name: "Java" },
-    { name: "Python" },
-    { name: "Kotlin" },
-    { name: "R" },
-    { name: "TypeScript" },
-    { name: "Next.js" },
-    { name: "WordPress" },
-    { name: "Bootstrap" },
-    { name: "Tailwind CSS" },
-    { name: "React Native" },
-    { name: "MySQL" },
-    { name: "MongoDB" },
-    { name: "Firebase" },
-    { name: "Oracle" },
-    { name: "Microsoft Azure" },
-    { name: "Android Studio" },
-    { name: "IoT" },
-    { name: "Arduino IDE" },
-    { name: "VMware" },
-    { name: "Git" },
-    { name: "Stripe" },
-    { name: "Razor" },
-    { name: "Power BI" },
-    { name: "Fedora" },
-    { name: "CentOS 7" },
-  
+    { name: "Java", image: javaImg },
+    { name: "React", image: reactImg },
+    { name: "JavaScript", image: javaScriptImg },
+    { name: "HTML", image: htmlImg },
+    { name: "PHP", image: phpImg },
+    { name: "MERN", image: mernImg },
+    { name: "C", image: cImg },
+    { name: "C++", image: cppImg },
+    { name: "C#", image: csharpImg },
+    { name: "Python", image: pythonImg },
+    { name: "Kotlin", image: kotlinImg },
+    { name: "R", image: rImg },
+    { name: "TypeScript", image: typescriptImg },
+    { name: "Next.js", image: nextjsImg },
+    { name: "WordPress", image: wordpressImg },
+    { name: "Bootstrap", image: bootstrapImg },
+    { name: "Tailwind CSS", image: tailwindImg },
+    { name: "React Native", image: reactNativeImg },
+    { name: "MySQL", image: mysqlImg },
+    { name: "MongoDB", image: mongodbImg },
+    { name: "Firebase", image: firebaseImg },
+    { name: "Oracle", image: oracleImg },
+    { name: "Microsoft Azure", image: azureImg },
+    { name: "Android Studio", image: androidStudioImg },
+    { name: "IoT", image: iotImg },
+    { name: "Arduino IDE", image: arduinoImg },
+    { name: "VMware", image: vmwareImg },
+    { name: "Git", image: gitImg },
+    { name: "Stripe", image: stripeImg },
+    { name: "Razor", image: razorImg },
+    { name: "Power BI", image: powerBIImg },
+    { name: "Fedora", image: fedoraImg },
+    { name: "CentOS 7", image: centosImg },
 ];
 
+
 export const Languages = () => {
-  const [isHovered, setIsHovered] = useState(false);
+
+  const variants = {
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0 }
+  };
+
 
   return (
     <section 
       className="skill mt-5" id="languages" 
-      onMouseEnter={() => setIsHovered(true)} 
-      onMouseLeave={() => setIsHovered(false)}
     >
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <div className="skill-bx wow zoomIn">
+          <motion.div 
+              className="skill-bx wow zoomIn" 
+              initial="hidden" 
+              whileInView="visible" 
+              variants={variants}
+              transition={{ duration: 0.8 }}// Adjust the duration as needed
+            >
+            <div className="skill-bx">
               <h2 className='mb-5'>Languages & Web Technologies</h2>
               <div className="row">
                 {skills.map((skill, index) => (
                   <div className="col-md-2 mb-4" key={index}>
-                    <motion.div
-                      initial={{ opacity: 0, y: 20 }}
-                      animate={{ opacity: isHovered ? 1 : 0.5, y: isHovered ? 0 : 20 }} // Change opacity and position on hover
-                      transition={{ duration: 0.5, delay: index * 0.1 }}
-                    >
-                      <Card style={{ width: '100%', borderRadius: '15px', textAlign: 'center' }}>
-                        <Card.Body>
-                          <Card.Title style={{ fontSize: '0.9rem' }} className='mt-2'>{skill.name}</Card.Title>
-                          
-                        </Card.Body>
-                      </Card>
-
-                    </motion.div>
+                    <Card style={{
+                      width: '100%',
+                      borderRadius: '15px',
+                      textAlign: 'center',
+                      color: 'white',
+                      background: 'radial-gradient(75% 63.6% at 50% 2.5%, rgb(36, 115, 236) 0%, rgba(99, 102, 241, 0) 100%)',
+                      
+                    }}>
+                      <Card.Body>
+                        <img src={skill.image} alt={skill.name} style={{ width: '50%', marginBottom: '10px' }} />
+                        <Card.Title style={{ fontSize: '0.9rem' }} className='mt-2'>{skill.name}</Card.Title>
+                      </Card.Body>
+                    </Card>
                   </div>
                 ))}
               </div>
             </div>
+            </motion.div>
           </div>
         </div>
       </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,25 +4,20 @@ import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png";
+import { motion } from 'framer-motion';
 
 export const Skills = () => {
   const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+    superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 5 },
+    desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3 },
+    tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
+    mobile: { breakpoint: { max: 464, min: 0 }, items: 1 }
+  };
+
+  // Define the scroll animations
+  const variants = {
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0 }
   };
 
   return (
@@ -30,10 +25,26 @@ export const Skills = () => {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <div className="skill-bx wow zoomIn">
+            <motion.div 
+              className="skill-bx wow zoomIn" 
+              initial="hidden" 
+              whileInView="visible" 
+              variants={variants}
+              transition={{ duration: 0.8 }}// Adjust the duration as needed
+            >
               <h2>Skills</h2>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br /> Lorem Ipsum has been the industry's standard dummy text.</p>
-              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                autoPlay={true}
+                autoPlaySpeed={1500}
+                transitionDuration={800}
+                customTransition="transform 800ms ease-in-out"
+                rtl={false}
+                arrows={false}
+                className="owl-carousel owl-theme skill-slider"
+              >
                 <div className="item">
                   <img src={meter1} alt="Web Development" />
                   <h5>Web Development</h5>
@@ -51,7 +62,7 @@ export const Skills = () => {
                   <h5>Web Development</h5>
                 </div>
               </Carousel>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
